0ef9a26c428bc760817e68bde7b4c37868fa0d01
alinuk1992 <ali.nourikhah@gmail.com>
2023-07-02 16:58:03 +0330

    initial commit

---
+++ src/Requests.js
@@ -0,0 +1,11 @@
+const key = 'af4e647eafa463f354f683dbd2101ca8';
+
+const requests = {
+  requestPopular: `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=1`,
+  requestTopRated: `https://api.themoviedb.org/3/movie/top_rated?api_key=${key}&language=en-US&page=1`,
+  requestTrending: `https://api.themoviedb.org/3/trending/all/day?api_key=${key}`,
+  requestTopRatedTv: `https://api.themoviedb.org/3/tv/top_rated?api_key=${key}&language=en-US&page=1`,
+  requestUpcoming: `https://api.themoviedb.org/3/movie/upcoming?api_key=${key}&language=en-US&page=1`,
+};
+
+export default requests;
